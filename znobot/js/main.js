4a0a09582ee6707cee2c417cd304737e2dee2c20
sashaadazhii <sashki.sashka@gmail.com>
2020-02-11 22:44:29 +0200

    without:mobile,btn hover,anchors

--- znobot/js/main.js
+++ znobot/js/main.js
@@ -1,5 +1,21 @@
 $(document).ready(function () {
 
+    //anchor landing smooth
+    $('a[href^="#"]').on('click', function (event) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 800);
+        }
+    });
+
+    /*top*/
+    $("#toTop").click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 800);
+    });
+
     // $("#btn-secondary").hover(function () {
     //     //mouse over
     //     $(this).removeClass('btn--transparent')
