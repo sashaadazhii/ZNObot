52a2fc26d36d617b3968ae4327fdd5c034bf72c4
sashaadazhii <sashki.sashka@gmail.com>
2020-02-20 10:46:35 +0200

    test block with vue

--- znobot/js/questions.js
+++ znobot/js/questions.js
@@ -1,12 +1,21 @@
 var app = new Vue({
     el: '#app',
     data: {
-        questions: {},
+        id: 526,
+        content: "Не мають закінчення обидва слова в рядку",
+        choices: [
+            { id: 0, content: "А: ухвалено, збудований", is_correct: false },
+            { id: 1, content: "Б: сорок, по-полтавськи", is_correct: false },
+            { id: 2, content: "В: горілиць, плакучі", is_correct: false },
+            { id: 3, content: "Г: запанібрата, змито", is_correct: true }],
+
+        getQuestion: false,
+        answer: null,
     },
 
     methods: {
         getTest() {
-
+            this.getQuestion = true;
 
 
         },
