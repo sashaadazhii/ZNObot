851afa6525c0889e0229f777e726b194f5677d7b
sashaadazhii <sashki.sashka@gmail.com>
2020-02-26 21:58:25 +0200

    dlt comments

--- znobot/js/main.js
+++ znobot/js/main.js
@@ -18,16 +18,13 @@ $(document).ready(function () {
 
     /*mobile 100vh */
     var containerWidth = $('body').width();
-    // var $header = $('.main-header');
     if (containerWidth <= 992) {
         let vh = window.innerHeight * 0.01;
         document.documentElement.style.setProperty('--vh', `${vh}px`);
 
         window.addEventListener('resize', () => {
-            // We execute the same script as before
             let vh = window.innerHeight * 0.01;
             document.documentElement.style.setProperty('--vh', `${vh}px`);
         });
-    }
-
+    };
 });
