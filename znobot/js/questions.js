f9f5da9c5856525ca8eb1148c2c2f4531c488337
sashaadazhii <sashki.sashka@gmail.com>
2020-02-29 10:25:58 +0200

    random question without default yet

--- znobot/js/questions.js
+++ znobot/js/questions.js
@@ -1,14 +1,23 @@
 var app = new Vue({
     el: '#app',
     data: {
-        id: 526,
-        content: "Не мають закінчення обидва слова в рядку",
-        choices: [
-            { id: 0, content: "А: ухвалено, збудований", is_correct: false },
-            { id: 1, content: "Б: сорок, по-полтавськи", is_correct: false },
-            { id: 2, content: "В: горілиць, плакучі", is_correct: false },
-            { id: 3, content: "Г: запанібрата, змито", is_correct: true }],
-
+        content: '',
+        choices: [],
+        getQuestion: false,
         answer: null,
     },
+
+    mounted() {
+        this.getQuestion = true;
+
+        this.$http
+            .get('http://zno-dev.eu-central-1.elasticbeanstalk.com/questions/random?subject=ukr')
+            .then((response) => {
+                this.content = response.data.content;
+                this.choices = response.data.choices;
+
+                console.log(response.data);
+            });
+    },
+
 })
