d4bf9eee15c4d6ac834fc457d6ec8c7f5903ca82
sashaadazhii <sashki.sashka@gmail.com>
2020-07-08 09:37:18 +0300

    question js and html changed

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 [Link to ZNObot](http://sashaadazhii.github.io/znobot/znobot/index.html)
+
+[Link to ZNObot (vue cli, firebase)](https://github.com/sashaadazhii/znobot-vuecli)
--- js/questions.js
+++ js/questions.js
@@ -1,39 +1,49 @@
 var app = new Vue({
     el: '#app',
     data: {
-        content: '',
+        content: "",
         choices: [],
-        getQuestion: false,
-        answer: null,
+        userAnswer: null,
         falseURL: false,
 
         defaultContent: "Не мають закінчення обидва слова в рядку",
-        defaultChoices: [
-            { id: 0, content: "*А*: ухвалено, збудований", is_correct: false },
-            { id: 1, content: "*Б*: сорок, по-полтавськи", is_correct: false },
-            { id: 2, content: "*В*: горілиць, плакучі", is_correct: false },
-            { id: 3, content: "*Г*: запанібрата, змито", is_correct: true }],
+        defaultChoices: [{
+                id: 0,
+                content: "*А*: ухвалено, збудований",
+                is_correct: false
+            },
+            {
+                id: 1,
+                content: "*Б*: сорок, по-полтавськи",
+                is_correct: false
+            },
+            {
+                id: 2,
+                content: "*В*: горілиць, плакучі",
+                is_correct: false
+            },
+            {
+                id: 3,
+                content: "*Г*: запанібрата, змито",
+                is_correct: true
+            }
+        ],
     },
 
     mounted() {
-        this.getQuestion = true;
-        var url = 'http://zno-dev.eu-central-1.elasticbeanstalk.com/questions/random?subject=ukr';
+        var url =
+            "http://zno-dev.eu-central-1.elasticbeanstalk.com/questions/random?subject=ukr";
 
-        this.$http      // axios
+        this.$http
             .get(url)
-            .then((response) => {
-
+            .then(response => {
                 this.content = response.data.content;
                 this.choices = response.data.choices;
-                console.log(response.data);
+                // console.log(response.data);
             })
-            .catch(function (error) {
-                if (error.status != 200) {
-                    this.falseURL = true;
-                    this.content = this.defaultContent;
-                    this.choices - this.defaultChoices;
-                }
-                return Promise.reject(error);
-            })
+            .catch(error => {
+                this.falseURL = true;
+                console.log(error);
+            });
     },
 })
